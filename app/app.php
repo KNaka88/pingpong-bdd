1467073f020274a831a76bcb61f6ae19a85f64e6
David Quisenberry and Koji Nakagawa <koji@>
2017-02-13 10:59:43 -0800

    create form input and ui for pingpong app

--- app/app.php
+++ app/app.php
@@ -10,9 +10,16 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         "twig.path" => __DIR__.'/../views'
     ));
+    $app['debug'] = true;
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('form.html.twig');
+    });
+
+    $app->get("/display_pingpong", function() use ($app) {
+        $new_ping_pong = new PingPongGenerator;
+        $number = $new_ping_pong->generatePingPongArray(intval($_GET['number']));
+        return $app['twig']->render('display_pingpong.html.twig', array('results' => $number));
     });
 
 
