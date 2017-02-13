ff639f45cc0c1ad66a35c0d63f670ea194b43c9b
David Quisenberry and Koji Nakagawa <david@>
2017-02-13 10:13:19 -0800

    change generatePingPongArray func to pass the first test: check if it is int

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -4,6 +4,8 @@
     {
         function generatePingPongArray($number)
         {
-
+            if(!is_int($number)){
+                return "non integer number entered";
+            }
         }
     }
